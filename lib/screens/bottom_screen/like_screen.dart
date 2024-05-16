42b122b7bd2047225fb97b3904a8eb9c429b2c5b
oceanmtechanshraiyani <anshrayani.oceanmtech@gmail.com>
2024-05-16 15:22:52 +0530

    Update like_screen.dart

---
+++ lib/screens/bottom_screen/like_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LikeScreen extends StatefulWidget {
+  const LikeScreen({super.key});
+
+  @override
+  State<LikeScreen> createState() => _LikeScreenState();
+}
+
+class _LikeScreenState extends State<LikeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
